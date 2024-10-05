fae3aa32ace8970bdfea0a140749dbbafa401f05
LokiSharp <loki.sharp@gmail.com>
2024-10-05 23:34:15 +0800

    feat: Add dev-tools direnv and vscode

--- home/base/gui/dev-tools.nix
+++ home/base/gui/dev-tools.nix
@@ -2,4 +2,26 @@
   home.packages = with pkgs; [
     sqlite
   ];
+
+  programs = {
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      enableNushellIntegration = true;
+    };
+  };
+
+  programs.vscode = {
+    package = pkgs.vscode;
+    enable = true;
+    extensions = (with pkgs;
+      with vscode-extensions; [
+        rust-lang.rust-analyzer
+        ms-vscode.cpptools
+        ms-vscode-remote.remote-ssh
+      ]);
+  };
 }

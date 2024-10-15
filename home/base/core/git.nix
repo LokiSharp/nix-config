ca53737b451b556a836c5c5cc06acfff2e6ff74e
LokiSharp <loki.sharp@gmail.com>
2024-10-16 01:15:26 +0800

    feat: 更新 git 配置

--- home/base/core/git.nix
+++ home/base/core/git.nix
@@ -4,8 +4,11 @@
   #
   #    https://git-scm.com/docs/git-config#Documentation/git-config.txt---global
   home.activation.removeExistingGitconfig = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
-    rm -f ~/.gitconfig
+    rm -f ${config.home.homeDirectory}/.gitconfig
   '';
+
+  home.packages = with pkgs; [
+  ];
 
   programs.git = {
     enable = true;
@@ -24,6 +27,7 @@
 
     extraConfig = {
       init.defaultBranch = "main";
+      trim.bases = "develop,master,main"; # for git-trim
       push.autoSetupRemote = true;
       pull.rebase = true;
 
@@ -46,10 +50,15 @@
     #   signByDefault = true;
     # };
 
+    # A syntax-highlighting pager in Rust(2019 ~ Now)
     delta = {
       enable = true;
       options = {
-        features = "side-by-side";
+        diff-so-fancy = true;
+        line-numbers = true;
+        true-color = "always";
+        # features => named groups of settings, used to keep related settings organized
+        # features = "";
       };
     };
 
@@ -60,10 +69,21 @@
       st = "status";
       ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
       ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
-      cm = "commit -m";
-      ca = "commit -am";
+      cm = "commit -m"; # commit via `git cm <message>`
+      ca = "commit -am"; # commit all changes via `git ca <message>`
       dc = "diff --cached";
-      amend = "commit --amend -m";
+
+      amend = "commit --amend -m"; # amend commit message via `git amend <message>`
+      unstage = "reset HEAD --"; # unstage file via `git unstage <file>`
+      merged = "branch --merged"; # list merged(into HEAD) branches via `git merged`
+      unmerged = "branch --no-merged"; # list unmerged(into HEAD) branches via `git unmerged`
+      nonexist = "remote prune origin --dry-run"; # list non-exist(remote) branches via `git nonexist`
+
+      # delete merged branches except master & dev & staging
+      #  `!` indicates it's a shell script, not a git subcommand
+      delmerged = ''! git branch --merged | egrep -v "(^\*|main|master|dev|staging)" | xargs git branch -d'';
+      # delete non-exist(remote) branches
+      delnonexist = "remote prune origin";
 
       # aliases for submodule
       update = "submodule update --init --recursive";
--- modules/base.nix
+++ modules/base.nix
@@ -1,6 +1,5 @@
 { pkgs
 , myvars
-, nuenv
 , nixpkgs
 , lib
 , ...

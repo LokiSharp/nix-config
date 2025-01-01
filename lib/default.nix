3f35ff61139ab053677e1662595c1373b0d09803
LokiSharp <loki.sharp@gmail.com>
2025-01-01 20:54:21 +0800

    refactor: 整理 lib 目录

--- lib/default.nix
+++ lib/default.nix
@@ -1,28 +1,53 @@
-{ lib, ... }: {
-  colmenaSystem = import ./colmenaSystem.nix;
-  nixosSystem = import ./nixosSystem.nix;
+{ config ? { }
+, pkgs ? { }
+, lib ? pkgs.lib
+, self ? null
+, hostsBase ? ../hosts
+, ...
+}:
+let
+  call =
+    path:
+    builtins.removeAttrs (lib.callPackageWith (pkgs // helpers) path { }) [
+      "override"
+      "overrideDerivation"
+    ];
+  helpers = rec {
+    inherit
+      config
+      pkgs
+      lib
+      hostsBase
+      ;
+    hosts = import ./fn/hosts.nix;
+    this = hosts."${config.networking.hostName}";
 
-  attrs = import ./attrs.nix { inherit lib; };
+    colmenaSystem = import ./system/colmenaSystem.nix;
+    nixosSystem = import ./system/nixosSystem.nix;
 
-  genK3sServerModule = import ./genK3sServerModule.nix;
-  genK3sAgentModule = import ./genK3sAgentModule.nix;
-  genKubeVirtHostModule = import ./genKubeVirtHostModule.nix;
-  genKubeVirtGuestModule = import ./genKubeVirtGuestModule.nix;
+    attrs = import ./fn/attrs.nix { inherit lib; };
 
-  # use path relative to the root of the project
-  relativeToRoot = lib.path.append ../.;
-  scanPaths = path:
-    builtins.map
-      (f: (path + "/${f}"))
-      (builtins.attrNames
-        (lib.attrsets.filterAttrs
-          (
-            path: _type:
-              (_type == "directory") # include directories
-              || (
-                (path != "default.nix") # ignore default.nix
-                && (lib.strings.hasSuffix ".nix" path) # include .nix files
-              )
-          )
-          (builtins.readDir path)));
-}
+    genK3sServerModule = import ./gen-k3s/genK3sServerModule.nix;
+    genK3sAgentModule = import ./gen-k3s/genK3sAgentModule.nix;
+    genKubeVirtHostModule = import ./gen-k3s/genKubeVirtHostModule.nix;
+    genKubeVirtGuestModule = import ./gen-k3s/genKubeVirtGuestModule.nix;
+
+    # use path relative to the root of the project
+    relativeToRoot = lib.path.append ../.;
+    scanPaths = path:
+      builtins.map
+        (f: (path + "/${f}"))
+        (builtins.attrNames
+          (lib.attrsets.filterAttrs
+            (
+              path: _type:
+                (_type == "directory") # include directories
+                || (
+                  (path != "default.nix") # ignore default.nix
+                  && (lib.strings.hasSuffix ".nix" path) # include .nix files
+                )
+            )
+            (builtins.readDir path)));
+  };
+in
+helpers

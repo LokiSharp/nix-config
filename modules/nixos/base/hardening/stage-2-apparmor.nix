f1009ece02088112f2099b3875a83d41b867dc29
LokiSharp <loki.sharp@gmail.com>
2026-02-20 18:45:14 +0800

    feat(hardening): 为 sing-box 添加 AppArmor 配置文件 (使用新版 policies 选项)

--- modules/nixos/base/hardening/stage-2-apparmor.nix
+++ modules/nixos/base/hardening/stage-2-apparmor.nix
@@ -15,7 +15,52 @@ in
     security.apparmor = {
       enable = true;
       enableCache = true;
-      killUnconfinedConfinables = true;
+      killUnconfinedConfinables = false;
+      policies =
+        lib.optionalAttrs (config.services.sing-box.enable && config.sops.templates ? "sing-box.json")
+          {
+            sing-box = {
+              state = "complain";
+              profile = ''
+                #include <tunables/global>
+
+                ${pkgs.sing-box}/bin/sing-box {
+                  #include <abstractions/base>
+                  #include <abstractions/nameservice>
+                  #include <abstractions/openssl>
+
+                  capability net_admin,
+                  capability net_bind_service,
+                  capability net_raw,
+
+                  network inet,
+                  network inet6,
+                  network raw,
+
+                  # Allow reading from nix store
+                  /nix/store/** r,
+
+                  # Config file from sops-nix templates
+                  ${config.sops.templates."sing-box.json".path} r,
+
+                  # Common system files
+                  /etc/resolv.conf r,
+                  /etc/nsswitch.conf r,
+                  /etc/ssl/certs/** r,
+                  /etc/static/ssl/certs/** r,
+
+                  # Runtime and data directories
+                  /run/sing-box/** rw,
+                  /var/lib/sing-box/** rw,
+                  /proc/sys/net/ipv4/ip_forward r,
+                  /proc/sys/net/ipv6/conf/all/forwarding r,
+                  
+                  # Allow execution of itself (needed for some setups)
+                  ${pkgs.sing-box}/bin/sing-box mr,
+                }
+              '';
+            };
+          };
     };
   };
 }

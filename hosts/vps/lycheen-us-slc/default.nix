ffa6be1adf19ae0450b4c494ef848f85f4a351cc
LokiSharp <loki.sharp@gmail.com>
2026-07-14 23:14:09 +0800

    feat(apparmor): enforce sing-box on proxy nodes

--- hosts/vps/lycheen-us-slc/default.nix
+++ hosts/vps/lycheen-us-slc/default.nix
@@ -25,6 +25,7 @@ in
   modules.base.hardening."stage-2".enforceProfiles = [
     "named"
     "bird"
+    "sing-box"
     "zerotierone"
     "tailscale"
   ];
--- hosts/vps/vultr-jp/default.nix
+++ hosts/vps/vultr-jp/default.nix
@@ -28,6 +28,7 @@ in
   modules.base.hardening."stage-2".enforceProfiles = [
     "named"
     "bird"
+    "sing-box"
     "zerotierone"
     "tailscale"
   ];

5d012ca6898eb4dd2569da2d958533398a823973
LokiSharp <loki.sharp@gmail.com>
2026-07-27 14:30:37 +0800

    test(zerotier): remove redundant route assertions

--- outputs/x86_64-linux/tests/zerotier-controller-routes/expected.nix
+++ outputs/x86_64-linux/tests/zerotier-controller-routes/expected.nix
@@ -8,10 +8,6 @@
   serverDisabledDn42Absent = true;
   serverDisabledLokiNetAbsent = true;
   vmMemberPresent = true;
-  vmStaleMemberAbsent = true;
-  vmSlkPrefixPresent = true;
-  vmDisabledDn42Absent = true;
-  vmDisabledLokiNetAbsent = true;
   testDn42IPv4Present = true;
   testDn42IPv6Present = true;
   testLokiNetIPv6Present = true;
--- outputs/x86_64-linux/tests/zerotier-controller-routes/expr.nix
+++ outputs/x86_64-linux/tests/zerotier-controller-routes/expr.nix
@@ -35,10 +35,6 @@ in
       "198.18.0.11"
       "fdbc:f9dc:67ad::11"
     ];
-  vmStaleMemberAbsent = !builtins.hasAttr "a2444b031c" members;
-  vmSlkPrefixPresent = hasRoute "fdbc:f9dc:67ad:11::/64" "fdbc:f9dc:67ad::11";
-  vmDisabledDn42Absent = !hasTarget "fd6a:11d4:cacb:11::1/128";
-  vmDisabledLokiNetAbsent = !hasTarget "2a0e:aa07:e220:11::1/128";
   testDn42IPv4Present = hasRoute "172.20.190.10/32" "198.18.0.10";
   testDn42IPv6Present = hasRoute "fd6a:11d4:cacb::10/128" "fdbc:f9dc:67ad::10";
   testLokiNetIPv6Present = hasRoute "2a0e:aa07:e220:10::1/128" "fdbc:f9dc:67ad::10";

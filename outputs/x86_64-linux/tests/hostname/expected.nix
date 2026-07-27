e97a7693cb5b25894f0e7d40df9e77c1bd47e2b8
LokiSharp <loki.sharp@gmail.com>
2026-07-27 14:31:47 +0800

    test(hostname): remove obsolete VM special case

--- outputs/x86_64-linux/tests/hostname/expected.nix
+++ outputs/x86_64-linux/tests/hostname/expected.nix
@@ -2,15 +2,4 @@
 , outputs
 ,
 }:
-let
-  specialExpected = {
-    "VM-NixOS" = "VM-NixOS";
-  };
-  specialHostNames = builtins.attrNames specialExpected;
-
-  otherHosts = builtins.removeAttrs outputs.nixosConfigurations specialHostNames;
-  otherHostsNames = builtins.attrNames otherHosts;
-  # other hosts's hostName is the same as the nixosConfigurations name
-  otherExpected = lib.genAttrs otherHostsNames (name: name);
-in
-(specialExpected // otherExpected)
+lib.genAttrs (builtins.attrNames outputs.nixosConfigurations) (name: name)
